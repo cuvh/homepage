f6208c175612c23697aa731431ffc537321e650c
Viktor Kirilov <deepsybg@gmail.com>
2015-10-15 17:16:28 +0300

    Remove unused files

--- tools/TODO.md
+++
@@ -1,7 +0,0 @@
-# TODO
-
-* Date archives
-* Sitemap task
-* Yeoman generator
-* Easy node module upgrades
-* Related posts
--- tools/gulpfile.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    "use strict";
-
-    var requireDir = require("require-dir");
-
-    // require all tasks in gulp/tasks, including sub-folders
-    requireDir("./gulp/tasks", { recurse: true });
-})();
