7965c9aba471f87a25310f23778704adad98c08c
Viktor Kirilov <Viktor Kirilov>
2015-11-08 17:38:15 +0200

    Optimize images now

--- tools/tasks/image-min.js
+++ tools/tasks/image-min.js
@@ -2,9 +2,11 @@
     'use strict';
 
     var gulp = require('gulp');
+	var image = require('gulp-image');
 
     gulp.task('image-min', function () {
         return gulp.src('./src/images/**/*.{png,jpg,jpeg,gif,svg}')
+       		.pipe(image())
             .pipe(gulp.dest('./build/images'));
     });
 })();
