e0fe72e248cf787a4603610ea649c60df7a62e10
ginovski <sashko.ginovski@gmail.com>
2015-12-09 16:29:58 +0200

    Bug fixings

--- src/js/about.js
+++ src/js/about.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+      var $windowWidth = $(window).width();
+      if ($windowWidth <= 768) {
+        $('.open-job-positions').html('Apply');
+      } else {
+        $('.open-job-positions').html('Apply at work@enhancv.com');
+      }
+
+      $(window).resize(function() {
+          var $windowWidth = $(window).width();
+          if ($windowWidth <= 768) {
+            $('.open-job-positions').html('Apply');
+          } else {
+            $('.open-job-positions').html('Apply at work@enhancv.com');
+          }
+      });
+
       $('.member-wrapper').mouseover(function () {
         var i, length,
           $this = $(this),
@@ -6,14 +23,14 @@ $(document).ready(function () {
           length = $childrenOfThis.length,
           $lastChildElement = $($childrenOfThis[length - 1]),
           $childrenOfLastChildElement = $lastChildElement.children();
-        
+
         for (i = 0; i < length- 1; i += 1) {
           var $currentElement = $($childrenOfThis[i]);
           $currentElement
             .css('visibility', 'hidden')
             .css('opacity', '0');
         }
-        
+
         $lastChildElement
           .css('visibility', 'visible')
           .css('opacity', '1');
@@ -21,7 +38,7 @@ $(document).ready(function () {
           .css('visibility', 'visible')
           .css('opacity', '1');
       });
-      
+
       $('.member-wrapper').mouseleave(function () {
         var i, length,
           $this = $(this),
@@ -29,14 +46,14 @@ $(document).ready(function () {
           length = $childrenOfThis.length,
           $lastChildElement = $($childrenOfThis[length - 1]),
           $childrenOfLastChildElement = $lastChildElement.children();
-        
+
         for (i = 0; i < length- 1; i += 1) {
           var $currentElement = $($childrenOfThis[i]);
           $currentElement
             .css('visibility', 'visible')
             .css('opacity', '1');
         }
-        
+
         $lastChildElement
           .css('visibility', 'hidden')
           .css('opacity', '0');
