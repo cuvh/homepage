6361b8b16c6623d986e98b157415106a39db6671
Krasimir Stoyanov <krasimir641@gmail.com>
2015-12-16 13:31:59 +0200

    Changed the behaviour when leaving page to fade

--- src/js/fading-content.js
+++ src/js/fading-content.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
   $('body').css('opacity', 1);
-  $('body a').click(function () {
-    $('body').css('opacity', 0);
-  });
-  $('body a.menu-hamb').click(function () {
-    $('body').css('opacity', 1);
-  });
-  $('body a.menu-close').click(function () {
-    $('body').css('opacity', 1);
-  });
+});
+
+$(window).on('beforeunload', function () {
+  $('body').css('opacity', 0);
 });
