13f6c7f47a441147b037f417f553f167e8241ccc
Viktor Kirilov <deepsybg@gmail.com>
2016-07-06 14:28:07 +0300

    Update deploy_s3.sh

--- bin/deploy_s3.sh
+++ bin/deploy_s3.sh
@@ -2,7 +2,7 @@ echo "[PRODUCTION DEPLOY] Installing AWS CLI..."
 pip install --user awscli
 
 echo "[PRODUCTION DEPLOY] Uploading HTMLS to S3..."
-aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*.js.map" --exclude "*.html" --cache-control "public, max-age=31"
+aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*.js.map" --exclude "*.html" --cache-control "public, max-age=15592000"
 
 echo "[PRODUCTION DEPLOY] Uploading media to S3..."
-aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*" --include "*.html" --cache-control "public, max-age=30"
+aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*" --include "*.html" --cache-control "public, max-age=300"
