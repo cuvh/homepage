bbe6f85851b40d062de4cf2d43dcee7832e7aaa1
Viktor Kirilov <deepsybg@gmail.com>
2016-07-06 14:23:03 +0300

    Update deploy_s3.sh

--- bin/deploy_s3.sh
+++ bin/deploy_s3.sh
@@ -1,16 +1,8 @@
-mkdir cdn_htmls
-cd cdn
-find . -name '*.html' -exec cp --parents \{\} ../cdn_htmls \;
-find . -name '*.html' -exec rm \{\} \;
-cd ..
-find cdn_htmls
-find cdn
-
 echo "[PRODUCTION DEPLOY] Installing AWS CLI..."
 pip install --user awscli
 
 echo "[PRODUCTION DEPLOY] Uploading HTMLS to S3..."
-aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*.js.map" --cache-control "public, max-age=15"
+aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*.js.map" --exclude "*.html" --cache-control "public, max-age=31"
 
 echo "[PRODUCTION DEPLOY] Uploading media to S3..."
-aws s3 sync ./cdn_htmls s3://$AWSBUCKET --acl public-read --cache-control "public, max-age=51"
+aws s3 sync ./cdn s3://$AWSBUCKET --acl public-read --exclude "*" --include "*.html" --cache-control "public, max-age=30"
