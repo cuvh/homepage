c4c74c7ed2c0c5a93819f381b84b368e9c256841
Slav Dachev <dachev@enhancv.com>
2016-08-18 13:45:48 +0300

    Fix examples page tests and update FAQ page texts

--- tests/integration/examples-test.js
+++ tests/integration/examples-test.js
@@ -1,4 +1,4 @@
-casper.test.begin('Examples page', 8, function (test) {
+casper.test.begin('Examples page', 7, function (test) {
     casper.start('http://127.0.0.1:8080/examples.html', function () {
 
         var resumes = [
