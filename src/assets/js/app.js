49bf82e7adc1059b5a1d5438f6341c88af412650
Ivan Kerin <ikerin@gmail.com>
2016-12-14 17:59:29 +0200

    Show first dot initially

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,8 +7,14 @@ $(function () {
         $('body').css('cursor', 'pointer');
         $('[data-toggle="popover"]').popover({ trigger: 'hover' });
     } else {
-        $('[data-toggle="popover"]').popover({ trigger: 'focus' });
+        $('[data-toggle="popover"]')
+            .popover({ trigger: 'focus' })
+            .first().popover('show');
     }
+
+    $(document).on('show.bs.popover', function (event) {
+        $('[data-toggle="popover"]').not(event.target).popover('hide');
+    });
 
     $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' });
 
