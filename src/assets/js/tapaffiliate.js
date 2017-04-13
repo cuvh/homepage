21c4e979b5bcac40e9b52949d7cfa89b1f487005
Ivan Kerin <ikerin@gmail.com>
2017-04-13 10:46:09 +0300

    Deploy tapaffiliate

--- src/assets/js/tapaffiliate.js
+++ src/assets/js/tapaffiliate.js
@@ -3,5 +3,5 @@ window['TapfiliateObject'] = i;
 window[i] = window[i] || function () {
     (window[i].q = window[i].q || []).push(arguments);
 };
-tap('create', '3155-aa5881');
+tap('create', process.env.TAPFILIATE_ID);
 tap('detectClick');
