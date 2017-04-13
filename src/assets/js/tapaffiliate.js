6b23c10d7afdfaa988fcca1a0c7c5dc616abd6bc
Ivan Kerin <ikerin@gmail.com>
2017-04-13 10:43:04 +0300

    Remove build folder (should not be in git)

---
+++ src/assets/js/tapaffiliate.js
@@ -0,0 +1,7 @@
+const i = 'tap';
+window['TapfiliateObject'] = i;
+window[i] = window[i] || function () {
+    (window[i].q = window[i].q || []).push(arguments);
+};
+tap('create', '3155-aa5881');
+tap('detectClick');
