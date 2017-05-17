196c90ea128b92e0810d257b9d7e1cce4efd9a7b
Slav Dachev <slav.dachev89@gmail.com>
2017-05-17 10:10:40 +0300

    Update tapaffiliate.js

--- src/assets/js/tapaffiliate.js
+++ src/assets/js/tapaffiliate.js
@@ -4,4 +4,13 @@ window[i] = window[i] || function () {
     (window[i].q = window[i].q || []).push(arguments);
 };
 tap('create', process.env.TAPFILIATE_ID);
-tap('detectClick');
+tap('detectClick', function (res) {
+    if (res && res.vid) {
+        var cookieName = 'tapfiliate_vid';
+        var cookieValue = res.vid;
+        var myDate = new Date();
+        myDate.setMonth(myDate.getMonth() + 12);
+        document.cookie = cookieName +"=" + cookieValue + ";expires=" + myDate 
+                  + ";domain=.enhancv.com;path=/";
+    }
+});
