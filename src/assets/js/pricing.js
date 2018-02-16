65a4b58d354391cdd1d64fd066f611b05a624af0
Viktor Kirilov <deepsybg@gmail.com>
2018-02-16 17:54:05 +0200

    initial work to new pricing

---
+++ src/assets/js/pricing.js
@@ -0,0 +1,28 @@
+(function() {
+	function positionPricingScrollbar() {
+		var overflowContainer = $(".overflow-wrapper-container");
+		var overflow = $(".overflow-wrapper");
+
+		if (overflow && overflowContainer) {
+			var scrollLeft = (overflow.width() - overflowContainer.width()) / 2;
+			overflowContainer.scrollLeft(scrollLeft);
+		}
+	}
+
+	$(window).on("resize", positionPricingScrollbar);
+
+	$(document).on("click", ".switch-basic", function() {
+		$(".switch-basic").addClass("focused");
+		$(".switch-pro").removeClass("focused");
+		$(".basic-tabs").fadeIn();
+		$(".pro-tabs").fadeOut();
+	});
+
+	$(document).on("click", ".switch-pro", function() {
+		$(".switch-pro").addClass("focused");
+		$(".switch-basic").removeClass("focused");
+		$(".basic-tabs").fadeOut();
+		$(".pro-tabs").fadeIn();
+		positionPricingScrollbar();
+	});
+})();
