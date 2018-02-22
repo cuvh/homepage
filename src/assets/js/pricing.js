b3572e8850003b298633f50464b07b8554a7bede
Viktor Kirilov <deepsybg@gmail.com>
2018-02-22 15:11:28 +0200

    add responsive image, update analytics, update scroll

--- src/assets/js/pricing.js
+++ src/assets/js/pricing.js
@@ -1,11 +1,13 @@
 (function() {
-	function positionPricingScrollbar() {
-		var overflowContainer = $(".overflow-wrapper-container");
-		var overflow = $(".overflow-wrapper");
+	var SCROLL_TIME = 300;
 
-		if (overflow && overflowContainer) {
-			var scrollLeft = (overflow.width() - overflowContainer.width()) / 2;
-			overflowContainer.scrollLeft(scrollLeft);
+	var container = $(".overflow-wrapper-container");
+	var content = $(".overflow-wrapper");
+
+	function positionPricingScrollbar() {
+		if (content && container) {
+			var scrollLeft = (content.width() - container.width()) / 2;
+			container.scrollLeft(scrollLeft);
 		}
 	}
 
@@ -15,7 +17,7 @@
 		$(".basic-tabs").fadeIn();
 		$(".pro-tabs").fadeOut();
 		$(".basic-benefits").show();
-		$(".pro-benefits").hide();
+		$(".pro-benefits, .scroll-dot-container").hide();
 	}
 
 	function switchPro() {
@@ -24,7 +26,7 @@
 		$(".basic-tabs").fadeOut();
 		$(".pro-tabs").fadeIn();
 		$(".basic-benefits").hide();
-		$(".pro-benefits").show();
+		$(".pro-benefits, .scroll-dot-container").show();
 		positionPricingScrollbar();
 	}
 
@@ -32,6 +34,60 @@
 	$(document).on("click", ".switch-pro", switchPro);
 	$(window).on("resize", positionPricingScrollbar);
 	positionPricingScrollbar();
+
+	var scrolled = false;
+	function detectScroll() {
+		if (scrolled) {
+			return;
+		}
+
+		window.dataLayer.push({
+			event: "UAEvent",
+			eventCategory: "Pricing V1",
+			eventAction: "Scroll plans",
+		});
+		scrolled = true;
+	}
+
+	container.on("scroll", function() {
+		var overflowContainer = container.width();
+		var overflow = content.width();
+
+		var currentScroll = container.scrollLeft();
+
+		var breakPoint = (overflow - overflowContainer) / 3;
+
+		$(".scroll-dot").removeClass("active");
+
+		if (currentScroll < breakPoint) {
+			$("#dot1").addClass("active");
+			detectScroll();
+		} else if (currentScroll > breakPoint && breakPoint * 2 > currentScroll) {
+			$("#dot2").addClass("active");
+		} else {
+			$("#dot3").addClass("active");
+			detectScroll();
+		}
+	});
+
+	$(".scroll-dot").click(function() {
+		var index = $(this).data("id");
+
+		if (index === 0) {
+			container.animate({ scrollLeft: 0 }, SCROLL_TIME);
+		}
+
+		if (index === 1) {
+			container.animate(
+				{ scrollLeft: (content.width() - container.width()) / 2 },
+				SCROLL_TIME
+			);
+		}
+
+		if (index === 2) {
+			container.animate({ scrollLeft: content.width() - container.width() }, SCROLL_TIME);
+		}
+	});
 
 	if (window.location.hash) {
 		if (window.location.hash.indexOf("basic") !== -1) {
