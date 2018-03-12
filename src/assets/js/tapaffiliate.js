646e14940de245b9f2481f89e72189ded3315834
Aleksandar Ginovski <sashko.ginovski@gmail.com>
2018-03-12 17:52:50 +0200

    Remove const

--- src/assets/js/tapaffiliate.js
+++ src/assets/js/tapaffiliate.js
@@ -1,7 +1,9 @@
-const i = 'tap';
-window['TapfiliateObject'] = i;
-window[i] = window[i] || function () {
-    (window[i].q = window[i].q || []).push(arguments);
-};
-tap('create', process.env.TAPFILIATE_ID);
-tap('detectClick');
+(function() {
+    var i = 'tap';
+    window['TapfiliateObject'] = i;
+    window[i] = window[i] || function () {
+        (window[i].q = window[i].q || []).push(arguments);
+    };
+    tap('create', process.env.TAPFILIATE_ID);
+    tap('detectClick');
+}());
