8272c2c3e4b6695b4e4c854bc63bd969f05e3397
Viktor Kirilov <deepsybg@gmail.com>
2018-06-08 18:40:03 +0300

    temporary fix modal

--- src/components/About/Member.jsx
+++ src/components/About/Member.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import Modal from "components/Modal";
+const Modal = ({ children }) => children;
 
 export default class Member extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			modalShowed: false,
+			modalShowed: false
 		};
 	}
 
 	onClickShowModal() {
 		this.setState({
-			modalShowed: true,
+			modalShowed: true
 		});
 	}
 
 	onClickHiddenModal() {
 		this.setState({
-			modalShowed: false,
+			modalShowed: false
 		});
 	}
 
@@ -35,25 +35,35 @@ export default class Member extends React.PureComponent {
 				 data-category="About"
 				 data-action="Click Team Member"
 				 data-label={this.props.name}
-				 onClick={() => this.onClickShowModal()}>
+				 onClick={() => this.onClickShowModal()}
+				>
 					<div className="member-text">
-						<p className="member-title bottom-xs">{this.props.name}</p>
-						<p className="no-margin text-muted">{this.props.position}</p>
+						<p className="member-title bottom-xs">
+							{this.props.name}
+						</p>
+						<p className="no-margin text-muted">
+							{this.props.position}
+						</p>
 					</div>
 				</a>
 
 				<Modal modalShowed={this.state.modalShowed}>
 					<div
 					 tabIndex="-1"
-					 className={this.state.modalShowed ? "modal in" : "modal fade"}>
+					 className={
+						this.state.modalShowed ? "modal in" : "modal fade"
+					 }
+					>
 						<div
 						 id={`${this.props.id}`}
-						 className="modal-dialog member-modal">
+						 className="modal-dialog member-modal"
+						>
 							<div className="modal-content">
 								<div className="embed-responsive embed-responsive-member">
 									<div
 									 style={{
-										backgroundImage: `url(${this.props.image})`,
+										backgroundImage: `url(${this.props
+											.image})`
 									 }}
 									 className="embed-responsive-item"
 									/>
@@ -62,14 +72,17 @@ export default class Member extends React.PureComponent {
 								 type="button"
 								 className="close"
 								 data-dismiss="modal"
-								 onClick={() => this.onClickHiddenModal()}>
+								 onClick={() => this.onClickHiddenModal()}
+								>
 									<span aria-hidden="true">&times;</span>
 								</button>
 
 								<div className="modal-body">
 									<h5 className="h5">{this.props.name}</h5>
 
-									<p className="text-primary">{this.props.position}</p>
+									<p className="text-primary">
+										{this.props.position}
+									</p>
 
 									{this.props.description ? (
 										<p>{this.props.description}</p>
@@ -78,9 +91,11 @@ export default class Member extends React.PureComponent {
 									<div className="top-sm">
 										{this.props.pdf ? (
 											<a
-											 href={`/img/about/member/${this.props.pdf}`}
+											 href={`/img/about/member/${this
+												.props.pdf}`}
 											 className="btn btn-default right-xs"
-											 target="_blank">
+											 target="_blank"
+											>
 												View Resume
 											</a>
 										) : null}
@@ -89,71 +104,85 @@ export default class Member extends React.PureComponent {
 											<a
 											 href={this.props.website}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-link" />
 											</a>
 										) : null}
 
 										{this.props.linkedin ? (
 											<a
-											 href={`https://www.linkedin.com/in/${this.props
-												.linkedin}`}
+											 href={`https://www.linkedin.com/in/${this
+												.props.linkedin}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-linkedin-alt" />
 											</a>
 										) : null}
 
 										{this.props.medium ? (
 											<a
-											 href={`https://medium.com/@${this.props.medium}`}
+											 href={`https://medium.com/@${this
+												.props.medium}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-social-medium" />
 											</a>
 										) : null}
 
 										{this.props.dribbble ? (
 											<a
-											 href={`https://dribbble.com/${this.props.dribbble}`}
+											 href={`https://dribbble.com/${this
+												.props.dribbble}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-dribbble" />
 											</a>
 										) : null}
 
 										{this.props.github ? (
 											<a
-											 href={`https://github.com/${this.props.github}`}
+											 href={`https://github.com/${this
+												.props.github}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-github-alt" />
 											</a>
 										) : null}
 
 										{this.props.twitter ? (
 											<a
-											 href={`https://twitter.com/${this.props.twitter}`}
+											 href={`https://twitter.com/${this
+												.props.twitter}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-twitter" />
 											</a>
 										) : null}
 
 										{this.props.facebook ? (
 											<a
-											 href={`https://facebook.com/${this.props.facebook}`}
+											 href={`https://facebook.com/${this
+												.props.facebook}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-facebook" />
 											</a>
 										) : null}
 
 										{this.props.instagram ? (
 											<a
-											 href={`https://instagram.com/${this.props.instagram}`}
+											 href={`https://instagram.com/${this
+												.props.instagram}`}
 											 className="btn btn-link btn-member-icon"
-											 target="_blank">
+											 target="_blank"
+											>
 												<i className="icon-instagram" />
 											</a>
 										) : null}
