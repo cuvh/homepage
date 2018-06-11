54a1ea6dca7241a4a597a84ab38f7f8fbae1ceda
Aleksandar Ginovski <sashko.ginovski@gmail.com>
2018-06-11 15:02:24 +0300

    Resume preview

--- src/components/SuccessfulResumes/Layouts/SingleResume.jsx
+++ src/components/SuccessfulResumes/Layouts/SingleResume.jsx
@@ -5,125 +5,159 @@ import intercomLogo from "assets/img/new-successful-resumes/highlighted/intercom
 import hannah from "assets/img/new-successful-resumes/hannah.png";
 import imgPlaceholder from "assets/img/new-successful-resumes/placeholders/img-placeholder.png";
 import FooterList from "components/SuccessfulResumes/FooterList";
+import ResumePreview from "components/SuccessfulResumes/ResumePreview";
 import SubscribeNoImage from "components/SuccessfulResumes/SubscribeNoImage";
 import herResume from "assets/img/new-successful-resumes/placeholders/her-resume.png";
 
 import DefaultLayout from "layouts/DefaultLayout";
 
-export default function SingleResume({
-    pathContext: { title, description, steps },
-    data: { userResumesJson: { ...data } },
-    ...rest
-}) {
-    return (
-        <DefaultLayout>
-            <div className="hasBubbulesBackground">
-                <main className="container">
-                    <section className="resumes--accent isSingle noBackground Grid full-width m-sm-top-3 m-xs-top-5 m-sm-top-5 m-md-top-6 m-sm-bottom-4 m-md-bottom-20">
-                        <div className="resumes--content Grid-cell--md-6 Grid-cell--xs-12 m-sm-bottom-3">
-                            <div className="text">
-                                <span className="text-tag text-highlight-purple">
-                                    CAREER CHANGE
+export default class SingleResume extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            preview: false
+        };
+    }
+
+    togglePreview() {
+        this.setState({ preview: !this.state.preview });
+    }
+
+    onKeyUp(e) {
+        if (e.keyCode === 27 && this.state.preview) {
+            this.togglePreview();
+        }
+    }
+
+    render() {
+        const { title, description, steps } = this.props.pathContext;
+        const data = this.props.data.userResumesJson;
+
+        return (
+            <DefaultLayout>
+                <div
+                 className="hasBubbulesBackground"
+                 onKeyUp={e => this.onKeyUp(e)}
+                >
+                    <main className="container">
+                        <section className="resumes--accent isSingle noBackground Grid full-width m-sm-top-3 m-xs-top-5 m-sm-top-5 m-md-top-6 m-sm-bottom-4 m-md-bottom-20">
+                            <div className="resumes--content Grid-cell--md-6 Grid-cell--xs-12 m-sm-bottom-3">
+                                <div className="text">
+                                    <span className="text-tag text-highlight-purple">
+                                        CAREER CHANGE
+                                    </span>
+                                    <h1 className="h1">{data.title}</h1>
+
+                                    <div className="m-xs-top-1 m-md-top-3">
+                                        <h5 className="h5 text-gray-light m-bottom-3">
+                                            <span className="text-gray-light">
+                                                A new resume changed her career
+                                                and her life
+                                            </span>
+                                        </h5>
+
+                                        <p className="p-big">
+                                            {data.description}
+                                        </p>
+                                    </div>
+                                    <a
+                                     onClick={() => this.togglePreview()}
+                                     target="_blank"
+                                     className="btn btn-big btn-primary m-xs-top-2 m-md-top-6"
+                                    >
+                                        See the resume
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="resumes--preview isSingle Grid-cell--md-6 Grid-cell--xs-12">
+                                <span className="resumes--preview-holder">
+                                    <a
+                                     onClick={() => this.togglePreview()}
+                                     data-track="event"
+                                     data-category="Successful Resumes"
+                                     data-action="Click Full Resume"
+                                     data-label="Casey Neistat"
+                                    >
+                                        <Img
+                                         resolutions={
+                                            data.image.childImageSharp.small
+                                         }
+                                        />
+
+                                        <button className="btn-resume-preview" />
+                                    </a>
                                 </span>
-                                <h1 className="h1">{data.title}</h1>
 
-                                <div className="m-xs-top-1 m-md-top-3">
-                                    <h5 className="h5 text-gray-light m-bottom-3">
-                                        <span className="text-gray-light">
-                                            A new resume changed her career and
-                                            her life
+                                <div className="resumes--accent-hired p-5">
+                                    <div className="Grid">
+                                        <h5>{data.name}’s career</h5>
+                                        <span className="label m-bottom-3">
+                                            Director, Banking, Tech, YouTube
+                                            personality, Marketing person
                                         </span>
-                                    </h5>
-
-                                    <p className="p-big">{data.description}</p>
-                                </div>
-                                <a
-                                 target="_blank"
-                                 href="#"
-                                 className="btn btn-big btn-primary m-xs-top-2 m-md-top-6"
-                                >
-                                    See the resume
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="resumes--preview isSingle Grid-cell--md-6 Grid-cell--xs-12">
-                            <span className="resumes--preview-holder">
-                                <a
-                                 href="#view-resume"
-                                 data-toggle="modal"
-                                 data-track="event"
-                                 data-category="Successful Resumes"
-                                 data-action="Click Full Resume"
-                                 data-label="Casey Neistat"
-                                >
-                                    <Img
-                                     resolutions={
-                                        data.image.childImageSharp.resolutions
-                                     }
-                                    />
-
-                                    <button className="btn-resume-preview" />
-                                </a>
-                            </span>
-
-                            <div className="resumes--accent-hired p-5">
-                                <div className="Grid">
-                                    <h5>{data.name}’s career</h5>
-                                    <span className="label m-bottom-3">
-                                        Director, Banking, Tech, YouTube
-                                        personality, Marketing person
-                                    </span>
-                                    <span className="label hired-label">
-                                        Hired at
-                                    </span>
-                                    <Img
-                                     resolutions={
-                                        data.companyLogo.childImageSharp
-                                            .resolutions
-                                     }
-                                     alt="Intercom Company logo"
-                                    />
+                                        <span className="label hired-label">
+                                            Hired at
+                                        </span>
+                                        <Img
+                                         resolutions={
+                                            data.companyLogo.childImageSharp
+                                                .resolutions
+                                         }
+                                         alt="Intercom Company logo"
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <a
-                         href="#"
-                         className="page--scroll"
-                        >
-                            Read the story
-                        </a>
-                    </section>
+                            <a
+                             href="#"
+                             className="page--scroll"
+                            >
+                                Read the story
+                            </a>
+                        </section>
 
-                    <article className="resume-article p-md-top-8">
-                        <h3>{data.stepsTitle}</h3>
-                        <p>{data.stepsDescription}</p>
+                        <article className="resume-article p-md-top-8">
+                            <h3>{data.stepsTitle}</h3>
+                            <p>{data.stepsDescription}</p>
 
-                        <ul>
-                            {data.steps.map((step, i) => (
-                                <li key={i}>
-                                    <h4>
-                                        Step {i + 1}: {step.title}
-                                    </h4>
-                                    <p>{step.description}</p>
-                                </li>
-                            ))}
-                        </ul>
+                            <ul>
+                                {data.steps.map((step, i) => (
+                                    <li key={i}>
+                                        <h4>
+                                            Step {i + 1}: {step.title}
+                                        </h4>
+                                        <p>{step.description}</p>
+                                    </li>
+                                ))}
+                            </ul>
 
-                        <figure>
-                            <img
-                             src={imgPlaceholder}
-                             alt="Image Placeholder"
+                            <figure>
+                                <img
+                                 src={imgPlaceholder}
+                                 alt="Image Placeholder"
+                                />
+                            </figure>
+                        </article>
+
+                        <SubscribeNoImage />
+                        <FooterList />
+                        {this.state.preview ? (
+                            <ResumePreview
+                             resumePageOne={data.image}
+                             facebookText={data.facebookText}
+                             twitterText={data.twitterText}
+                             url={data.url}
+                             altText={data.altText}
+                             togglePreview={() => this.togglePreview()}
                             />
-                        </figure>
-                    </article>
-
-                    <SubscribeNoImage />
-                    <FooterList />
-                </main>
-            </div>
-        </DefaultLayout>
-    );
+                        ) : null}
+                    </main>
+                </div>
+            </DefaultLayout>
+        );
+    }
 }
 
 export const pageQuery = graphql`
@@ -140,7 +174,10 @@ export const pageQuery = graphql`
             }
             image {
                 childImageSharp {
-                    resolutions(width: 548) {
+                    large: resolutions(width: 1240) {
+                        ...GatsbyImageSharpResolutions
+                    }
+                    small: resolutions(width: 548) {
                         ...GatsbyImageSharpResolutions
                     }
                 }
---
+++ src/components/SuccessfulResumes/ResumePreview.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import SocialPanel from "./SocialPanel";
+import Img from "gatsby-image";
+
+export default function ResumePreview({
+    resumePageOne,
+    resumePageTwo,
+    altText,
+    url,
+    facebookText,
+    twitterText,
+    togglePreview
+}) {
+    return (
+        <div
+         id="view-resume"
+         tabIndex="-1"
+         className="modal"
+        >
+            <div className="modal-dialog modal-lg modal-example">
+                <div className="modal-content">
+                    <div className="modal-body">
+                        <Img
+                         resolutions={resumePageOne.childImageSharp.large}
+                         alt={altText}
+                         style={{ width: "100%", position: "initial" }}
+                         imgStyle={{ height: "auto", borderRadius: "5px" }}
+                        />
+                    </div>
+                    <SocialPanel
+                     className="inModal hasBackground"
+                     facebookText={facebookText}
+                     twitterText={twitterText}
+                     url={url}
+                    />
+                </div>
+            </div>
+            <button
+             type="button"
+             onClick={() => togglePreview()}
+             className="close close-example"
+            >
+                <i className="icon-times" />
+            </button>
+        </div>
+    );
+}
