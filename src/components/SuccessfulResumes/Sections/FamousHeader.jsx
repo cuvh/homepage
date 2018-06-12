edf50e702729ba5fd35041df5ca526129d93ca1e
Viktor Kirilov <deepsybg@gmail.com>
2018-06-12 13:29:29 +0300

    fix colors

--- src/components/SuccessfulResumes/Sections/FamousHeader.jsx
+++ src/components/SuccessfulResumes/Sections/FamousHeader.jsx
@@ -23,7 +23,7 @@ export default function FamousHeader({
             <div className="container">
                 <div className="resumes--content Grid-cell--lg-6 Grid-cell--md-5 Grid-cell--xs-12">
                     <div className="text text-dark-grey text-center-xs text-center-sm">
-                        <h1 className="h1 text-white">{name}</h1>
+                        <h1 className="h1 text-md-white">{name}</h1>
                         <div className="m-xs-top-1 m-md-top-3 text-darker-grey">
                             <h5 className="h5 text-white-smoke m-bottom-3">
                                 {smallDescription}
