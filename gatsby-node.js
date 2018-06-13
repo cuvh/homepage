cd484654f5f245b524586d3f1cc42463146d6507
Viktor Kirilov <deepsybg@gmail.com>
2018-06-13 16:00:54 +0300

    fix deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -74,3 +74,19 @@ exports.createPages = async ({ graphql, boundActionCreators }) => {
 		}
 	);
 };
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+	const timestamp = Date.now();
+	switch (stage) {
+		case "build-javascript":
+			config.merge({
+				output: {
+					filename: `[name]-${timestamp}-[chunkhash].js`,
+					chunkFilename: `[name]-${timestamp}-[chunkhash].js`
+				}
+			});
+
+			break;
+	}
+	return config;
+};
