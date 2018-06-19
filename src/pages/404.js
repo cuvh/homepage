dc5efbb1b4a6704f8d11221cd2e0a5237103789b
Viktor Kirilov <deepsybg@gmail.com>
2018-06-19 13:55:34 +0300

    fix pricing and cleanup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Link from "gatsby-link";
 
 export default function Page404() {
 	return (
 		<div className="container container-page page-not-found bottom-lg">
 			<h3 className="text-center">
-				Error 405<br />
-				<small>This is not the page you are looking for.</small>
+				Error 404<br />
+				<small>This is not the page you are looking for :/</small>
+				<br />
+				<Link
+				 className="btn btn-primary m-top-4"
+				 to="/">
+					Back to safety
+				</Link>
 			</h3>
 		</div>
 	);
