445839548980e15346c21cb239757906c02a3e49
Aleksandar Ginovski <sashko.ginovski@gmail.com>
2018-06-19 16:48:42 +0300

    Implement sticky w/ react

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -59,37 +59,6 @@ exports.onInitialClientRender = () => {
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
     })(window, document, "//static.hotjar.com/c/hotjar-", ".js?sv=");
-
-    (function() {
-        if (document.querySelector(".famous-resume--navbar")) {
-            var prevScrollpos = window.pageYOffset;
-            const NAV_HEIGHT = 62;
-            var famousNavInitialOffset =
-                document.querySelector(".famous-resume--navbar").offsetTop - NAV_HEIGHT;
-
-            window.onscroll = function() {
-                var currentScrollPos = window.pageYOffset;
-                var topNav = document.querySelector(".navbar-static-top");
-                var famousNav = document.querySelector(".famous-resume--navbar");
-
-                var distanceScrolled = prevScrollpos - currentScrollPos;
-
-                if (currentScrollPos > famousNavInitialOffset) {
-                    if (prevScrollpos > currentScrollPos) {
-                        topNav.style.top =
-                            Math.min(parseInt(topNav.style.top) + distanceScrolled, 0) + "px";
-                        famousNav.style.top = NAV_HEIGHT + parseInt(topNav.style.top) + "px";
-                    } else {
-                        topNav.style.top =
-                            famousNav.getBoundingClientRect().top - NAV_HEIGHT + "px";
-                        famousNav.style.top =
-                            Math.max(parseInt(famousNav.style.top) + distanceScrolled, 0) + "px";
-                    }
-                }
-                prevScrollpos = currentScrollPos;
-            };
-        }
-    })();
 };
 
 exports.onRouteUpdate = ({ location }) => {
--- src/components/SuccessfulResumes/Sections/Menu.jsx
+++ src/components/SuccessfulResumes/Sections/Menu.jsx
@@ -1,26 +1,82 @@
 import React from "react";
 
-export default function Menu({ sections }) {
-	return (
-		<section className="famous-resume--navbar m-sm-bottom-6">
-			<nav className="container">
-				<ul className="Grid text-center-xs text-center-sm">
-					{sections.map(section => (
-						<li
-						 key={section}
-						 className="Grid-cell--md-2 Grid-cell--xs-12 m-md-right-5"
-						>
-							<a href={`#${section}`}>
-								{section.replace(/([A-Z])/g, " $1")}
-							</a>
-						</li>
-					))}
+const NAV_HEIGHT = 62;
 
-					<li className="Grid-cell--md-2 Grid-cell--xs-12 m-md-right-5">
-						<a href="#famous-resume">Resume</a>
-					</li>
-				</ul>
-			</nav>
-		</section>
-	);
+export default class Menu extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offsetTop: 0,
+            scrollPosition: 0,
+        };
+
+        this.menu = React.createRef();
+        this.navbar = {};
+    }
+
+    componentDidMount() {
+        this.navbar = document.querySelector(".navbar-static-top");
+
+        this.setState({ scrollPosition: window.pageYOffset });
+        let famousNavInitialOffset = this.menu.current.offsetTop - NAV_HEIGHT;
+
+        window.addEventListener("scroll", () => this.scroll(famousNavInitialOffset));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", () => this.scroll());
+    }
+
+    scroll(famousNavInitialOffset) {
+        if (window.innerWidth >= 992) {
+            let menu = this.menu.current;
+            let currentScrollPos = window.pageYOffset;
+            let distanceScrolled = this.state.scrollPosition - currentScrollPos;
+
+            if (currentScrollPos > famousNavInitialOffset) {
+                if (this.state.scrollPosition > currentScrollPos) {
+                    this.navbar.style.top =
+                        Math.min(parseInt(this.navbar.style.top) + distanceScrolled, 0) + "px";
+                    this.setState({
+                        offsetTop: NAV_HEIGHT + parseInt(this.navbar.style.top) + "px",
+                    });
+                } else {
+                    this.navbar.style.top = menu.getBoundingClientRect().top - NAV_HEIGHT + "px";
+                    this.setState({
+                        offsetTop:
+                            Math.max(parseInt(this.state.offsetTop) + distanceScrolled, 0) + "px",
+                    });
+                }
+            }
+
+            this.setState({ scrollPosition: currentScrollPos });
+        }
+    }
+
+    render() {
+        const { sections } = this.props;
+
+        return (
+            <section
+             className="famous-resume--navbar m-sm-bottom-6"
+             ref={this.menu}
+             style={{ top: this.state.offsetTop }}>
+                <nav className="container">
+                    <ul className="Grid text-center-xs text-center-sm">
+                        {sections.map(section => (
+                            <li
+                             key={section}
+                             className="Grid-cell--md-2 Grid-cell--xs-12 m-md-right-5">
+                                <a href={`#${section}`}>{section.replace(/([A-Z])/g, " $1")}</a>
+                            </li>
+                        ))}
+
+                        <li className="Grid-cell--md-2 Grid-cell--xs-12 m-md-right-5">
+                            <a href="#famous-resume">Resume</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        );
+    }
 }
