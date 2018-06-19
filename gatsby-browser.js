166529070d879a22020f40465ee4e4ffafd42a59
Aleksandar Ginovski <sashko.ginovski@gmail.com>
2018-06-19 15:42:42 +0300

    Implement sticky navbar on famous resumes page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -23,7 +23,7 @@ exports.onInitialClientRender = () => {
         scale: 1,
         easing: "ease-out",
         distance: "50px",
-        viewFactor: browserWidth > 768 ? 0.6 : 0.45
+        viewFactor: browserWidth > 768 ? 0.6 : 0.45,
     });
 
     sr.reveal(".spotlight-animate", {
@@ -31,7 +31,7 @@ exports.onInitialClientRender = () => {
         easing: "ease-out",
         distance: "50px",
         delay: 500,
-        viewFactor: browserWidth() > 768 ? 0.6 : 0.45
+        viewFactor: browserWidth() > 768 ? 0.6 : 0.45,
     });
 
     var isDesktop = browserWidth() > 768;
@@ -42,7 +42,7 @@ exports.onInitialClientRender = () => {
             delay: isDesktop ? i * 100 : 0,
             easing: "ease-out",
             distance: "50px",
-            viewFactor: isDesktop ? 0.6 : 0.45
+            viewFactor: isDesktop ? 0.6 : 0.45,
         });
     }
 
@@ -59,6 +59,34 @@ exports.onInitialClientRender = () => {
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
     })(window, document, "//static.hotjar.com/c/hotjar-", ".js?sv=");
+
+    (function() {
+        var prevScrollpos = window.pageYOffset;
+        const NAV_HEIGHT = 62;
+        var famousNavInitialOffset =
+            document.querySelector(".famous-resume--navbar").offsetTop - NAV_HEIGHT;
+
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+            var topNav = document.querySelector(".navbar-static-top");
+            var famousNav = document.querySelector(".famous-resume--navbar");
+
+            var distanceScrolled = prevScrollpos - currentScrollPos;
+
+            if (currentScrollPos > famousNavInitialOffset) {
+                if (prevScrollpos > currentScrollPos) {
+                    topNav.style.top =
+                        Math.min(parseInt(topNav.style.top) + distanceScrolled, 0) + "px";
+                    famousNav.style.top = NAV_HEIGHT + parseInt(topNav.style.top) + "px";
+                } else {
+                    topNav.style.top = famousNav.getBoundingClientRect().top - NAV_HEIGHT + "px";
+                    famousNav.style.top =
+                        Math.max(parseInt(famousNav.style.top) + distanceScrolled, 0) + "px";
+                }
+            }
+            prevScrollpos = currentScrollPos;
+        };
+    })();
 };
 
 exports.onRouteUpdate = ({ location }) => {
