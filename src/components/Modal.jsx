25567f9a28445ecfa3e6053e3ace481bf217c9e1
Nikolay Likomanov <me@theniki.com>
2018-06-26 13:11:11 +0300

    Added Resume preview - Modal Footer Bar, Fixed modal close button for mobile and desktop, Fixed .modal overflow

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import bannerResumePreview from "assets/img/new-successful-resumes/small_banner_resume_preview_illustration@2.png";
+
 function Portal({ children }) {
     return typeof document !== "undefined"
         ? ReactDOM.createPortal(children, document.getElementById("___gatsby"))
@@ -48,6 +50,10 @@ export default class OpenModal extends React.PureComponent {
         document.body.classList.remove("no-scroll");
     }
 
+    onCloseFooterBar() {
+        document.querySelector('.modal-footer-bar').classList.add("hide");
+    }
+
     render() {
         const { opened } = this.state;
         const { trigger, children, additionalContent } = this.props;
@@ -75,7 +81,33 @@ export default class OpenModal extends React.PureComponent {
                             </button>
 
                             {additionalContent}
+
+                            <div className="modal-footer-bar">
+                                <div className="modal-footer-bar-wrap modal-lg Grid Grid--alignCenter">
+                                    <img 
+                                        src={bannerResumePreview}
+                                        className="m-right-2"
+                                        height="107"
+                                        alt="Footer Bar Illustration"/>
+                                    <div>
+                                    <h6 className="h6 text-dark-grey m-bottom-1">Your dream job awaits, make your move.</h6>
+                                    <p className="text-darker-grey medium">Create a resume to be proud of with Enhnacv.</p>
+                                    </div>
+                                    <a
+                                        href="https://app.enhancv.com/signup"
+                                        // onClick={() => Track("Modal", "Signup Header")}
+                                        className="btn btn-primary m-right-1">
+                                        Try it for free
+                                    </a>
+                                    <button 
+                                        onClick={() => this.onCloseFooterBar()}
+                                        className="close-modal-footer-bar">
+                                    </button>
+                                </div>
+                            </div>
+
                         </div>
+                        
                     </Portal>
                 ) : null}
             </React.Fragment>
